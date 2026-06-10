e441826884ae4797d03453223c110933e72a95b3
MollyCat <tomsteinbach@yahoo.com>
2026-06-11 01:03:42 +0200

    - Fixes

--- Game/story.py
+++ Game/story.py
@@ -1,6 +1,7 @@
 from rich.console import Console
 from rich.markdown import Markdown
 import classes
+from classes import *
 console = Console()
 
 '''
@@ -44,15 +45,17 @@ ein Schild erkennen: Morghorn.
 """)
 prologue = [prl1, prl2, prl3, prl4]
 
+wandererevent = 0
 def storyprologue(pl):
+    global wandererevent
     console.print(ttlcrd)
     for i in prologue:
         console.print("Prolog", style="bold underline yellow")
         console.print(i)
         classes.action1(pl)
-        prog = 0
-        prog += 1
-        if prog == 2:
+        wandererevent += 1
+        if wandererevent == 2:
             print("")
             console.print("Im dichten Nebel erkennst du eine Sillhouette...")
             print("")
+            wanderer(pl)
