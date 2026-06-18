8eb453c53cdf95268fd1f7bfeee23ede8b6afdef
MollyCat <tomsteinbach@yahoo.com>
2026-06-18 08:11:14 +0200

    Änderungen: - Anzeige ausgebessert - Story angepasst

--- main.py
+++ main.py
@@ -5,17 +5,13 @@ Main-Datei des Spiels
 
 # Import
 from rich.console import Console
-
 console = Console()
-console.print("rich loaded", style="green")
-console.print("classes loaded", style="green")
 import story
-console.print("story loaded", style="green")
 from classes import *
 import classes
-console.print("classes loaded", style="green")
 print("")
 print("")
+
 #Titelcard
 console.print("Lyrae The Game", style="bold underline yellow")
 console.print("by Tom Steinbach", style="italic magenta")
--- story.py
+++ story.py
@@ -6,43 +6,44 @@ console = Console()
 In dieser Datei befinden sich sämtliche Storyelemente
 '''
 
-ttlcrd = Markdown("""
-### Lyrae the Game
-""")
-
 
 prl1 = ("""+--------------------------------------------------------+
 | Du bewegst dich durch die eisigen Weiten               |
 | des großen Grallbag-Gebirges, auf der Spitze Lyraes.   |
 | In sehnsucht nach Wärme suchst du schon seit einem Tag |
 | nach einen Unterschlupf.                               |
-| Was sollst du nur tun?                                 |
+| Außer weiterzugehen, bleiben dir nicht viele Optionen. |
 +--------------------------------------------------------+\n""")
 
-prl2 = ("""+-------------------------------------------------------+
-| Hinter einer dichten Winddecke kannst du Licht in der |
-| Ferne erkennen. Du bekommst Hoffnung auf Obdach.      |
-+-------------------------------------------------------+\n""")
+prl2 = ("""+--------------------------------------------------------+
+| Entlang des Weges findest du einen Wegweiser mit       |
+| einer Weisung: "Morghorn".                             |
+| Der Frost auf deinem Körper treibt dich an,            |
+| die Richtung einzuschlagen. Die Reflexion der Sonne im |
+| brennt in deinen Augen, während du versuchst den Weg   |
+| im Schnee zu finden.                                   |
++--------------------------------------------------------+\n""")
 
-prl3 = (f"""+-----------------------------------------------------------------+
-| Die Lichter werden dir immer näher. Ist es ein Dorf? Ein Lager? |
-| Ein Feuer scheint es zumindest zu geben.                        |
-+-----------------------------------------------------------------+\n""")
+prl3 = (f"""+--------------------------------------------------------+
+| Fern durch den Nebel erkennst du flackernde Lichter.   |
+| Endlich findest du die Zuversicht nicht zu erfrieren!  |
+| Und natürlich ist das nicht dein Ende, du bist         |
+| immerhin der Protagonist!                              |
++--------------------------------------------------------+\n""")
 
-prl4 = ("""+-----------------------------------------------------------------+
-| Es ist ein Dorf! Entlang eines verschneiten Weges konntest du   |
-| ein Schild erkennen: Morghorn.                                  |
-+-----------------------------------------------------------------+\n""")
+prl4 = ("""+--------------------------------------------------------+
+|                              
++--------------------------------------------------------+\n""")
 prologue = [prl1, prl2, prl3, prl4]
 
 
 def storyprologue(pl):
-    console.print(ttlcrd)
+    console.print(f"                    Lyrae The Game", style = "red")
     event = 0
     eventchance = random.randint(1, 4)
     for i in prologue:
         time.sleep(0.1)
-        console.print("Prolog", style="bold underline yellow")
+        console.print("                         Prolog", style="bold underline yellow")
         console.print(i)
         classes.action1(pl)
         event += 1
